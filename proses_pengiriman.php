6f21676e0fe449e74af16a02fd66a72e3d56e56d
YuriFerdinand <muhammadyuri965@gmail.com>
2025-03-17 17:46:42 +0700

    Perbaikan (Memperbaiki proses pengiriman)

--- proses_pengiriman.php
+++ proses_pengiriman.php
@@ -4,10 +4,23 @@ session_start();
 
 if(isset($_POST['simpan'])) {
 
-    $nama = $_POST["nama"];
+    $pelangganId = $_SESSION["pelanggan_id"];
     $paket = $_POST["paket"];
     $desc = $_POST["deskripsi"];
 
-    $query = "INSERT INTO pengiriman (pelanggan_id, paket_id, deskripsi, tanggal_pengiriman) VALUES ('$nama','$paket','$desc',NOW())";
-    $exec = mysqli_query($conn, $query);
+    $query = "INSERT INTO pengiriman (pelanggan_id, paket_id, deskripsi, tanggal_pengiriman) VALUES ('$pelangganId','$paket','$desc',NOW())";
+
+    if ($conn->query($query) === TRUE) {
+        $_SESSION['notification'] = [
+            'type' => 'primary',
+            'message' => 'Berhasil menambahkan pengiriman.'
+        ];
+
+    }else {
+        $_SESSION['notification'] = [
+            'type' => 'danger',
+            'message' => 'Pengiriman gagal ditambahkan'
+        ];
+    }
+    header('Location: pengiriman.php');
 }
