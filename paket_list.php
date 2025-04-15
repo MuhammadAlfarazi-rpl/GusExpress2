1e0898fbb00da708dbe5b877d1fbeffcd755e2a9
YuriFerdinand <muhammadyuri965@gmail.com>
2025-04-15 20:50:21 +0700

    Edit (Menambahkan proses edit paket)

--- paket_list.php
+++ paket_list.php
@@ -64,7 +64,10 @@ include '.includes/header.php';
                                             <input type="hidden" name="paket_id" value="'.$paket['paket_id'].'">
                                         <button type="button" class="btn btn-primary"> <i class="bx bx-edit-alt"></i> Edit</button>
                                         <button name="delete" type="submit" class="btn btn-outline-danger"> <i class="bx bx-trash" ></i> Hapus Paket</button>
+                                        <a href="edit_paket.php?paket_id= '.$paket['paket_id'].'"><button type="button" class="btn btn-primary"> <i class="bx bx-edit-alt"></i> Edit</button>
                                         </form>
+                                        
+                                        
                                 </span>
                                 
                             </div>
