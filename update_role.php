dab3f74ec913bafa1b98b9872b0c428aaf87dae6
YuriFerdinand <muhammadyuri965@gmail.com>
2025-04-16 22:02:46 +0700

    Admin (Menambahkan proses untuk mengubah user menjadi admin)

--- update_role.php
+++ update_role.php
@@ -1,1 +1,14 @@
+<?php
+include 'config.php';
 
+$admin = "admin";
+
+$query = "UPDATE pelanggan SET role = '$admin'";
+$result = $conn->query($query);
+
+if (isset($_POST["ubah"])) {
+    $result;
+    header('Location: list_user.php');
+} else {
+    
+}
