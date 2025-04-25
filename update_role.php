f0e8ae1bcda8b97118d6da77f80aeb39080f9826
YuriFerdinand <muhammadyuri965@gmail.com>
2025-04-25 23:41:39 +0700

    Admin (Menambahkan fitur untuk menghapus dan mendemosi akun)

--- update_role.php
+++ update_role.php
@@ -2,7 +2,7 @@
 include 'config.php';
 session_start();
 
-if (isset($_POST["ubah"])) {
+if (isset($_POST["promote"])) {
     $admin = "admin";
     $pelangganID = $_POST['pelanggan_id'];
 
@@ -21,4 +21,63 @@ if (isset($_POST["ubah"])) {
         ];
     }
     header('Location: list_user.php');
+}
+
+if (isset($_POST["demote"])) {
+    $user = "user";
+    $pelangganID = $_POST['pelanggan_id'];
+
+    $sql = "UPDATE pelanggan SET role = '$user' WHERE pelanggan_id = '$pelangganID'";
+    $result = $conn->query($sql);
+
+    if($result) {
+        $_SESSION["notification"] = [
+            'type' => 'primary',
+            'message' => 'Demosi admin berhasil.'
+        ];
+    
+    } else {
+        $_SESSION["notification"] = [
+            'type' => 'danger',
+            'message' => 'Gagal mendemosi admin.'
+        ];
+    }
+    header('Location: list_user.php');
+}
+
+if (isset($_POST["delete"])) {
+    $pelangganID = $_POST['pelanggan_id'];
+
+    $cekAdmnQuery = "SELECT role FROM pelanggan WHERE pelanggan_id = '$pelangganID'";
+    $cekResult = $conn->query($cekAdmnQuery);
+
+
+    if ($cekResult && $cekResult->num_rows > 0) {
+        $cekAdmn = $cekResult->fetch_assoc();
+        if ($cekAdmn['role'] == 'admin') {
+            $_SESSION["notification"] = [
+                'type' => 'danger',
+                'message' => 'Tidak bisa menghapus akun admin.'
+            ];
+            header('Location: list_user.php');
+    } else {
+        $sql = "DELETE FROM pelanggan WHERE pelanggan_id = '$pelangganID'";
+        $result = $conn->query($sql);
+        if($result) {
+            $_SESSION["notification"] = [
+                'type' => 'primary',
+                'message' => 'Demosi admin berhasil.'
+            ];
+        
+        } else {
+            $_SESSION["notification"] = [
+                'type' => 'danger',
+                'message' => 'Gagal mendemosi admin.'
+            ];
+        }
+        header('Location: list_user.php');
+    }
+
+    }
+        
 }
