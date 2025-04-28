bae534b6c86262da1e61cd197e39f9cffd445ff0
MuhammadAlfarazi-rpl <mdrsff@gmail.com>
2025-04-28 22:38:12 +0700

    Harga (memperbarui tampilan harga ekspedisi)

--- paket.php
+++ paket.php
@@ -58,7 +58,7 @@ include '.includes/header.php';
                             $result = $conn->query($query);
                             if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
-                                    echo "<option value='" . $row["id_harga"] . "'>" . $row["nama_harga"] . "</option>";
+                                    echo "<option value='" . $row["id_harga"] . "'>" . $row["nama_harga"] ."--". $row["nama_harga_ekspedisi"]. "</option>";
                                 }
                         }
                         ?>
